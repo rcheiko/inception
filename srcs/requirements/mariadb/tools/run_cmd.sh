2ce4bd0f29d77f2c837db6115c5b17809c9d81e5
Romain Cheiko <rcheiko@c1r7p2.42nice.fr>
2022-03-23 12:08:17 +0100

    update

--- srcs/requirements/mariadb/tools/run_cmd.sh
+++ srcs/requirements/mariadb/tools/run_cmd.sh
@@ -2,8 +2,6 @@ service mysql start
 echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" | mysql
 echo "grant all on ${MYSQL_DATABASE}.* to '${MYSQL_USER}'@'%' identified by '${MYSQL_PASSWORD}';" | mysql
 echo "flush privileges;" | mysql
-echo "ALTER USER 'root'@'%' IDENTIFIED BY 'PASSWORD';" | mysql
-
 
 mysql wordpress < data.sql
 
